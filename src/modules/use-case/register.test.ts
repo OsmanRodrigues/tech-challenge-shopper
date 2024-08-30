eff2b5b830c00cef07b6cf2e3eb66fb11d531d73
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-08-30 10:22:39 -0300

    chore(tests): exception cases todo added

--- src/modules/use-case/register.test.ts
+++ src/modules/use-case/register.test.ts
@@ -1,10 +1,10 @@
-import { it, describe, before } from 'node:test';
+import { it, describe, before, todo } from 'node:test';
 import assert from 'node:assert';
+import fs from 'node:fs';
 import { MeasureRegisterRequestDTO } from '../model/dto';
 import { registerMeasureUseCase } from './register';
 import { startStorageServices } from '../external/storage';
 import { startAIServices } from '../external/ai';
-import fs from 'node:fs';
 
 //NOTE comment test image file or base64 for select base64 mock consume
 // const base64Mock = fs.readFileSync('test-sample.jpeg', 'base64');
@@ -16,20 +16,25 @@ describe('Use case: register measure', undefined, () => {
     startAIServices();
   });
 
-  it('Should register a measure', async () => {
-    const registerRequestPayload: MeasureRegisterRequestDTO = {
-      customer_code: '12345',
-      image: base64Mock,
-      measure_datetime: '2024-08-29T00:09:52.312315Z',
-      measure_type: 'WATER',
-    };
-    const registerResponse = await registerMeasureUseCase(
-      registerRequestPayload
-    );
+  // NOTE uncomment this block after excpetion cases are covered
+  // it('Should register a measure', async () => {
+  //   const registerRequestPayload: MeasureRegisterRequestDTO = {
+  //     customer_code: '12345',
+  //     image: base64Mock,
+  //     measure_datetime: '2024-08-29T00:09:52.312315Z',
+  //     measure_type: 'WATER',
+  //   };
+  //   const registerResponse = await registerMeasureUseCase(
+  //     registerRequestPayload
+  //   );
 
-    assert.ok(registerResponse);
-    assert.ok(registerResponse.image_url);
-    assert.ok(registerResponse.measure_uuid);
-    assert.ok(registerResponse.measure_value);
-  });
+  //   assert.ok(registerResponse);
+  //   assert.ok(registerResponse.image_url);
+  //   assert.ok(registerResponse.measure_uuid);
+  //   assert.ok(registerResponse.measure_value);
+  // });
+  todo('Should throw invalid data error once request payload has wrong data');
+  todo(
+    'Should throw double report error once already exist a measure for the current month'
+  );
 });
